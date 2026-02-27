ac1c45dc15e42152cee2905a71529f5bab959c5e
lp2165 <catrielcabrera97@gmail.com>
2026-02-27 14:18:13 -0300

    middlware3

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,17 @@
 import { auth } from "@/auth";
+import { NextResponse } from "next/server";
 
 export default auth((req) => {
   const isLoggedIn = !!req.auth;
   const isDashboard = req.nextUrl.pathname.startsWith("/dashboard");
 
   if (isDashboard && !isLoggedIn) {
-    return Response.redirect(new URL("/login", req.url));
+    return NextResponse.redirect(new URL("/login", req.url));
   }
+
+  return NextResponse.next();
 });
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/dashboard/:path*"],
 };
